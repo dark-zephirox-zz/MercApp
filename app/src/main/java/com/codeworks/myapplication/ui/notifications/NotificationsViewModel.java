c85274a7a075a133ded9f2d8ea01593532c76dd5
DarkSnow15 <ajperalta4@misena.edu.co>
2019-11-30 11:50:22 -0500

    Funcionalidad via App

--- app/src/main/java/com/codeworks/myapplication/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/codeworks/myapplication/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("Usted tiene actualmente {cant_value} productos en el carrito, por valor de {money_value}");
     }
 
     public LiveData<String> getText() {
